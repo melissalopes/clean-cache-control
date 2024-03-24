ccc744de09e2d86906350462ac81611b4ee2a2b4
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-03-23 22:52:39 -0300

    chore: jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+};

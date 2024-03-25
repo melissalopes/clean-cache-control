5b8d48a0b91cd0a0e67c9278650598f7922791c7
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-03-25 20:38:35 -0300

    refactor: merge testes

--- src/data/useCases/local-save-purchases.spec.ts
+++ src/data/useCases/local-save-purchases.spec.ts
@@ -50,11 +50,6 @@ describe('LocalSavePurchases', () => {
     const { sut, cacheStore } = makeSut();
     await sut.save();
     expect(cacheStore.deleteCallsCount).toBe(1);
-  });
-
-  test('Should call delete with correct key', async () => {
-    const { sut, cacheStore } = makeSut();
-    await sut.save();
     expect(cacheStore.key).toBe('purchases');
   });
 });

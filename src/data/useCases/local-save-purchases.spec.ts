4a5dd99a9ed80640c4ef47603893247760bb246b
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-03-25 19:40:56 -0300

    feat: teste para garantir que LocalSavePurchases exclua caches antigos quando salvar

--- src/data/useCases/local-save-purchases.spec.ts
+++ src/data/useCases/local-save-purchases.spec.ts
@@ -1,14 +1,24 @@
 /* classe de produção */
 class LocalSavePurchases {
   constructor(private readonly cacheStore: ICacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
 }
 
-interface ICacheStore {}
+interface ICacheStore {
+  delete: () => void;
+}
 /* classe de produção */
 
 /* mock do que está em produção */
 class CacheStoreSpy implements ICacheStore {
   deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
 /* mock do que está em produção */
 
@@ -17,5 +27,12 @@ describe('LocalSavePurchases', () => {
     const cacheStore = new CacheStoreSpy();
     new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test('Should delete old cache on save', async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
